5b947df4e6292af2d7453f94f583d1846e5129a2
anbere <90169033+anbere@users.noreply.github.com>
2021-10-22 09:27:59 -0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-//package com.example.demo;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//
-//@SpringBootApplication
-//public class DemoApplication {
-//
-//  public static void main(String[] args) {
-//    SpringApplication.run(DemoApplication.class, args);
-//  }
-//  /*@Autowired
-//  private UserRepository userRepository;
-//  @Autowired
-//  private AccountRepository accountRepository;
-//  @Override
-//  public void run(String... args) throws Exception {
-//    User anbers = new User(
-//            "anbers",
-//            "password",
-//            "andres@gmail.com",
-//            "Andres",
-//            "Rodriguez"
-//    );
-//
-//    User a = new User(
-//            "averagesizedRod",
-//            "password",
-//            "rodrigo@gmail.com",
-//            "Rodrigo",
-//            "Flores"
-//    );
-//
-//    Account account1 = new Account(543.75, "34523239");
-//    Account account2 = new Account(743.65, "43243123");
-//    anbers.setAccount(account1);
-//    account1.setUser(anbers);
-//
-//    a.setAccount(account2);
-//    account2.setUser(a);
-//    userRepository.save(anbers);
-//    userRepository.save(a);
-//
-//    accountRepository.save(account1);
-//    accountRepository.save(account2);
-//
-//    Transaction transaction = new Transaction("pay", 200, "testing if this sends", LocalDate.now(), "posted", account1.getId(), account2.getId());
-//    TransactionService transactionService = new TransactionService(transactionRepository);
-//    transactionService.addNewTransaction(transaction);
-//    transactionRepository.save(transaction);
-
-  // }
-=======
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
@@ -65,5 +10,4 @@ public class DemoApplication {
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
->>>>>>> andres
 }
